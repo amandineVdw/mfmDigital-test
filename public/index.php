bd70dc5ece5d2cfa36d70953e78b23e32e190a68
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2024-09-19 10:46:48 +0200

    css added

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+// public/index.php
+define('BASE_PATH', __DIR__ . '/../');
+
+
+// Activer la gestion des erreurs pour tout rediriger vers le fichier error.log
+ini_set('log_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('error_log', BASE_PATH.'logs/error.log'); // Rediriger les erreurs vers logs/error.log
+
+require_once BASE_PATH . 'src/controllers/ContactController.php';
+require_once BASE_PATH . 'src/models/Database.php';
+require_once BASE_PATH . 'src/error-handler.php';
+
+// Connexion à la base de données
+$database = new Database();
+$db = $database->getConnection();
+
+// Créer une instance du contrôleur ContactController
+$controller = new ContactController($db);
+
+// Router les requêtes selon l'action demandée
+$action = $_GET['action'] ?? 'showForm';
+
+if ($action === 'submitContact') {
+    $controller->submitContact();
+} else {
+    $controller->showForm();
+}
